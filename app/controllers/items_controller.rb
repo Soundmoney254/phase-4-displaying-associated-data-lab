b416c4fcab8fc887d0101a3b32431e90a7be062d
Samuel <samnmbuguah@gmail.com>
2023-07-01 12:53:30 +0300

    Completed Assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index
+        @items = Item.all
+        render json: @items.map { |item| item.as_json(except: [:created_at, :updated_at, :user_id], include: { user: { except: [:created_at, :updated_at] } }) }
+    end
+
+    private
+    def render_not_found_response
+    render json: { error: "Items not found" }, status: :not_found
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def show
+      @user = User.find(params[:id])
+      @items = @user.items
+      render json: {
+        id: @user.id,
+        username: @user.username,
+        city: @user.city,
+        items: @items.map { |item| item.as_json(except: [:created_at, :updated_at, :user_id]) }
+      }, except: [:created_at, :updated_at]
+    end
+  
+    private
+    def render_not_found_response
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+  
